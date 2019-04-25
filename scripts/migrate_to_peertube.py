cc009aadf3880705da6ede5e3d2979c323026c97
Yohan Boniface <yb@enix.org>
2019-04-25 18:09:05 +0200

    Peertube migration: comments and comments attachments

--- scripts/migrate_to_peertube.py
+++ scripts/migrate_to_peertube.py
@@ -23,10 +23,15 @@ ROOT = Path(".cache")
 KINTO_URL = "https://kinto.classea12.beta.gouv.fr"
 KINTO_USER = "classea12admin"
 KINTO_PASSWORD = os.environ.get("KINTO_PASSWORD", "")
-PEERTUBE_BASE_URL = os.environ.get("PEERTUBE_URL", "https://peertube.scopyleft.fr")
+PEERTUBE_BASE_URL = os.environ.get(
+    "PEERTUBE_URL", "https://peertube-staging.classe-a-12.beta.gouv.fr"
+)
 PEERTUBE_URL = f"{PEERTUBE_BASE_URL}/api/v1"
 PEERTUBE_USER = "classea12"
 PEERTUBE_PASSWORD = os.environ.get("PEERTUBE_PASSWORD", "")
+SCURIOLUS_URL = os.environ.get(
+    "SCURIOLUS_URL", "https://files-staging.classe-a-12.beta.gouv.fr/"
+)
 
 
 def urlretrieve(url, dest):
@@ -91,6 +96,10 @@ class Attachment:
 @dataclass
 class Resource:
     @classmethod
+    def get_root(cls):
+        return ROOT / cls.__name__.lower()
+
+    @classmethod
     def all(cls):
         for path in cls.get_root().glob("*-meta"):
             yield cls(**json.loads(path.read_text()))
@@ -106,7 +115,19 @@ class Resource:
 
 
 @dataclass
-class Video(Resource):
+class WithAttachment(Resource):
+    def __post_init__(self):
+        if self.attachment:
+            self.attachment = Attachment(**self.attachment)
+
+    def download(self, force=False):
+        super().download(force=force)
+        if self.attachment:
+            self.attachment.download(force=force)
+
+
+@dataclass
+class Video(WithAttachment):
     attachment: Attachment
     creation_date: int
     description: str
@@ -122,15 +143,8 @@ class Video(Resource):
     title: str
     quarantine: bool = False
 
-    def __post_init__(self):
-        self.attachment = Attachment(**self.attachment)
-
     def __str__(self):
         return self.title
-
-    @classmethod
-    def get_root(self):
-        return ROOT / "video"
 
     def download(self, force=False):
         super().download(force=force)
@@ -145,7 +159,6 @@ class Video(Resource):
                 Image.open(dest).convert("RGB").save(
                     dest, "JPEG", quality=95, subsampling=0
                 )
-        self.attachment.download(force=force)
 
     def get_thumbnail_file(self):
         return self.get_root() / f"{self.id}-thumbnail"
@@ -163,19 +176,31 @@ class Profile(Resource):
     id: str
     last_modified: int
 
-    @classmethod
-    def get_root(self):
-        return ROOT / "profile"
-
     @property
     def username(self):
         return self.email.split("@")[0].lower().replace("-", ".")
 
 
-@minicli.cli
-def pull(force=False):
+@dataclass
+class Comment(WithAttachment):
+    video: str
+    schema: int
+    comment: str
+    profile: str
+    id: str
+    last_modified: int
+    attachment: Attachment = None
+
+
+@lru_cache()
+def get_kinto_headers():
     token = b64encode(f"{KINTO_USER}:{KINTO_PASSWORD}".encode()).decode()
-    headers = {"Authorization": f"Basic {token}"}
+    return {"Authorization": f"Basic {token}"}
+
+
+@minicli.cli
+def pull_videos(force=False):
+    headers = get_kinto_headers()
     resp = requests.get(
         f"{KINTO_URL}/v1/buckets/classea12/collections/videos/records", headers=headers
     )
@@ -191,13 +216,31 @@ def pull(force=False):
     for raw in videos:
         video = Video(quarantine=True, **raw)
         video.download(force=force)
+
+
+@minicli.cli
+def pull_comments(force=False):
+    resp = requests.get(
+        f"{KINTO_URL}/v1/buckets/classea12/collections/comments/records",
+        headers=get_kinto_headers(),
+    )
+    for raw in resp.json()["data"]:
+        comment = Comment(**raw)
+        comment.download(force=force)
+
+
+@minicli.cli
+def pull_profiles(force=False):
+    headers = get_kinto_headers()
     resp = requests.get(f"{KINTO_URL}/v1/accounts", headers=headers)
     if not resp.ok:
         print(resp.text)
         return
     data = resp.json()["data"]
     for raw in data:
+        print(f"Pulling {raw['id']}")
         if not raw.get("validated") or not raw.get("profile"):
+            print("Skipping.")
             continue
         id_ = raw.get("profile")
         resp = requests.get(
@@ -213,6 +256,13 @@ def pull(force=False):
         profile.download(force=force)
 
 
+@minicli.cli
+def pull(force=False):
+    pull_videos(force=force)
+    pull_profiles(force=force)
+    pull_comments(force=force)
+
+
 @lru_cache()
 def get_client():
     resp = requests.get(f"{PEERTUBE_URL}/oauth-clients/local")
@@ -220,7 +270,7 @@ def get_client():
 
 
 @lru_cache()
-def get_peertube_token(user, password):
+def get_peertube_token(user, password=PEERTUBE_PASSWORD):
     client_id, client_secret = get_client()
     url = f"{PEERTUBE_URL}/users/token"
     resp = requests.post(
@@ -249,11 +299,12 @@ def get_channel_id(headers):
 def push(limit=1, skip_error=False):
     push_profiles(skip_error, limit)
     push_videos(skip_error, limit)
+    push_comments(skip_error, limit)
 
 
 @minicli.cli
 def push_profiles(skip_error=False, limit=1):
-    token = get_peertube_token(PEERTUBE_USER, PEERTUBE_PASSWORD)
+    token = get_peertube_token(PEERTUBE_USER)
     headers = {"Authorization": f"Bearer {token}"}
     count = 0
     for profile in Profile.all():
@@ -279,7 +330,7 @@ def push_profiles(skip_error=False, limit=1):
             break
         url = f"{PEERTUBE_URL}/users/me"
         data = {"displayName": profile.name.replace(".", " "), "bio": profile.bio}
-        user_token = get_peertube_token(profile.username, PEERTUBE_PASSWORD)
+        user_token = get_peertube_token(profile.username)
         user_headers = {"Authorization": f"Bearer {user_token}"}
         resp = requests.put(url, headers=user_headers, data=data)
         if not resp.ok:
@@ -294,12 +345,63 @@ def push_profiles(skip_error=False, limit=1):
 
 
 @minicli.cli
+def push_comments(skip_error=False, limit=1):
+    profiles = {p.id: p for p in Profile.all()}
+    admin_token = get_peertube_token(PEERTUBE_USER)
+    admin_headers = {"Authorization": f"Bearer {admin_token}"}
+    count = 0
+    for comment in Comment.all():
+        video_id = MAPPING[comment.video]
+        if comment.id in MAPPING:
+            remote_id = MAPPING[comment.id]
+            resp = requests.get(
+                f"{PEERTUBE_URL}/videos/{video_id}/comment-threads/{remote_id}",
+                headers=admin_headers,
+            )
+            if resp.ok:
+                print("Comment already in the remote server", video_id, remote_id)
+                continue
+        profile = profiles[comment.profile]
+        token = get_peertube_token(profile.username)
+        headers = {"Authorization": f"Bearer {token}"}
+        print(video_id, profile.username)
+        url = f"{PEERTUBE_URL}/videos/{video_id}/comment-threads"
+        resp = requests.post(url, {"text": comment.comment}, headers=headers)
+        if not resp.ok:
+            if skip_error:
+                continue
+            print(resp.content)
+            breakpoint()
+            break
+        remote_id = resp.json()["comment"]["id"]
+        print(f"Created comment on video {video_id} as thread {remote_id}")
+        MAPPING[comment.id] = remote_id
+        if comment.attachment:
+            filename = comment.attachment.get_filename()
+            path = f"/{video_id}/{remote_id}/{filename}"
+            url = f"{SCURIOLUS_URL}{path}"
+            resp = requests.put(
+                url, data=open(comment.attachment.get_file(), "rb"), headers=headers
+            )
+            if not resp.ok:
+                if skip_error:
+                    continue
+                print(resp.content)
+                breakpoint()
+                break
+            print(f"Pushed attachment to {url}")
+        count += 1
+        if limit and count >= limit:
+            break
+
+
+@minicli.cli
 def push_videos(skip_error=False, limit=1, video_id=None):
     profiles = {p.email: p for p in Profile.all()}
     ownership = json.loads((ROOT / "mapping_video_user.json").read_text())
     url = f"{PEERTUBE_URL}/videos/upload"
     count = 0
-    admin_token = get_peertube_token(PEERTUBE_USER, PEERTUBE_PASSWORD)
+    admin_token = get_peertube_token(PEERTUBE_USER)
     admin_headers = {"Authorization": f"Bearer {admin_token}"}
     for video in sorted(Video.all(), key=lambda v: v.publish_date):
         if video_id and not video.id == video_id:
@@ -322,7 +424,7 @@ def push_videos(skip_error=False, limit=1, video_id=None):
             else:
                 print(f"Owner not found for {email}")
         print(f"Using user {user}")
-        token = get_peertube_token(user, PEERTUBE_PASSWORD)
+        token = get_peertube_token(user)
         headers = {"Authorization": f"Bearer {token}"}
         channel_id = get_channel_id(headers)
         data = {
